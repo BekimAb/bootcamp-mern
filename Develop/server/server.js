2da25e1ae554dd20812492874d20546c7928f3fc
bekim abedini <daabedini22@gmail.com>
2022-08-09 20:39:26 -0400

    update

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const path = require('path');
+
+const{ApolloServer}=require('apollo-server-express');
+const{typeDefs,resolvers}=require('./schemas')
+const{authMiddleware}=require('./utils/auth');
+
 const db = require('./config/connection');
 const routes = require('./routes');
+
+//apollo server
+const server=new ApolloServer({
+  typeDefs,
+  resolvers,
+  context:authMiddleware
+});
+//apollo server with express
+server.applyMiddleware({app});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +30,11 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+app.get('*',(req,res)=>{
+  res.sendfile(path.join(__dirname, '../client/build/index.html'));
+})
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log('use graphql at http://localhost:${PORT}${server.graphqlPath}');
 });
